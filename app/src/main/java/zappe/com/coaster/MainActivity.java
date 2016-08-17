bb693ee74b67c12056e4062bfd654bdad014086c
Jannik Zappe <jannik.zappe@udo.edu>
2016-08-17 23:05:34 +0200

    new layout with fragments

--- app/src/main/java/zappe/com/coaster/MainActivity.java
+++ app/src/main/java/zappe/com/coaster/MainActivity.java
@@ -1,40 +1,39 @@
 package zappe.com.coaster;
 
-import android.content.Intent;
-import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
+import android.app.DialogFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.Toast;
+import android.widget.TextView;
+
 import java.text.NumberFormat;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    Button addDrinkButton;
+    TextView subtotal;
+    TextView tip;
+    TextView total;
 
     DrinkHolder drinks;
     public DrinkAdapter adapter;
     SQLiteDatabaseHelper sqLiteDatabaseHelper;
 
-    public final static int ADD_RESULT = 1;
-    public final static int EDIT_RESULT = 2;
-    public final static int DELETE_RESULT = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        addDrinkButton = (Button) findViewById(R.id.addDrinkButton);
-
         sqLiteDatabaseHelper = new SQLiteDatabaseHelper(this);
 
         drinks = new DrinkHolder(this, sqLiteDatabaseHelper);
+
+        subtotal = (TextView) findViewById(R.id.subtotal);
+        tip = (TextView) findViewById(R.id.tip);
+        total = (TextView) findViewById(R.id.total);
 
         GridView drinkGrid = (GridView) findViewById(R.id.drinkGrid);
         if(drinkGrid != null) {
@@ -45,62 +44,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addDrink(View view) {
-        Intent intent = new Intent(this, AddActivity.class);
-        startActivityForResult(intent, ADD_RESULT);
+        FragmentManager manager = this.getFragmentManager();
+        FragmentTransaction fragmentTransaction = manager.beginTransaction();
+        Fragment previousFragment = manager.findFragmentByTag("add_drink");
+        if (previousFragment != null) {
+            fragmentTransaction.remove(previousFragment);
+        }
+
+        DialogFragment editFragment = new AddFragment();
+        editFragment.show(fragmentTransaction, "add_drink");
     }
 
-    private void calculateTotals() {
-        Resources res = getResources();
-
+    public void calculateTotals() {
+        double subtotalAmount = drinks.getSubtotal();
         double totalAmount = drinks.getTotal();
-        double totalWithoutTipAmount = drinks.getTotalWithoutTip();
 
-        String total = res.getString(R.string.total);
+        String subtotalAmountString = NumberFormat.getCurrencyInstance().format(subtotalAmount);
+        String tipString = NumberFormat.getCurrencyInstance().format(totalAmount-subtotalAmount);
+        String totalAmountString = NumberFormat.getCurrencyInstance().format(totalAmount);
 
-        String totalAmountString = NumberFormat.getCurrencyInstance().format(totalWithoutTipAmount);
-        String totalWithTipAmountString = NumberFormat.getCurrencyInstance().format(totalAmount);
-
-        setTitle(total+" "+totalAmountString+" ("+totalWithTipAmountString+")");
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    public void paid(MenuItem item) {
-        ArrayList<DrinkModel> drinksArrayList = this.drinks.getDrinks();
-        for (int i=drinksArrayList.size()-1; i >= 0; i--) {
-            drinks.remove(i);
-        }
-        notifyChanged();
-
-        Toast.makeText(this, R.string.everything_paid, Toast.LENGTH_LONG).show();
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (data != null) {
-            DrinkModel drink = (DrinkModel) data.getExtras().getSerializable("drink");
-            int position = data.getIntExtra("position", -1);
-
-            if (drink != null) {
-                switch (resultCode) {
-                    case ADD_RESULT:
-                        drinks.add(drink.name, drink.amount, drink.price);
-                        break;
-                    case EDIT_RESULT:
-                        drinks.update(position, drink);
-                        break;
-                    case DELETE_RESULT:
-                        drinks.remove(position);
-                        break;
-                }
-                adapter.notifyDataSetChanged();
-                calculateTotals();
-            }
-        }
+        subtotal.setText(subtotalAmountString);
+        tip.setText(tipString);
+        total.setText(totalAmountString);
     }
 
     public void notifyChanged() {
