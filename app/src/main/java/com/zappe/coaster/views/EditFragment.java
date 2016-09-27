bce496af95621c071f53f46153945d50ae23d840
Jannik Zappe <jannik.zappe@udo.edu>
2016-09-27 18:34:44 +0200

    Dialog erledigt

--- app/src/main/java/com/zappe/coaster/views/EditFragment.java
+++ app/src/main/java/com/zappe/coaster/views/EditFragment.java
@@ -1,10 +1,10 @@
 package com.zappe.coaster.views;
 
-import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
